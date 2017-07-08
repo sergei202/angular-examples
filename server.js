be9eb13384fd1185fd65356cb9fc91c078c13f7c
Sergei <sergei@sleekbook>
2017-07-08 08:40:34 -0500

    Add http example.

--- server.js
+++ server.js
@@ -13,3 +13,16 @@ app.use('/', express.static('./public'));
 app.listen(3000, function(err) {
 	console.log('Listening at http://localhost:3000');
 });
+
+
+/*********************************************
+Everything below this is for the $http example
+*********************************************/
+
+app.get('/list', function(req,res) {
+	res.json(['car','red','apples']);
+});
+
+app.get('/random', function(req,res) {
+	res.json(Math.random()*100);
+});
